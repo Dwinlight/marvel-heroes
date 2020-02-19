cf795ef2a84d1e8ea80210398f8c654a115b57e5
Dwinlight <corentinpouget15@gmail.com>
2020-02-19 12:28:49 +0100

    correction import

--- scripts/import-mongo.js
+++ scripts/import-mongo.js
@@ -16,54 +16,54 @@ const insertHeroes = (db, callback) => {
         // Pour chaque ligne on créé un document JSON pour l'acteur correspondant
         .on('data', data => {
 
-            heroes.push({
+        heroes.push({
 
-                "id":data.id,
-                "name":data.name,
-                "description":data.description,
-                "imageUrl":data.imageUrl,
-                "backgroundImageUrl":data.backgroundImageUrl,
-                "externalLink":data.externalLink,
-                "identity":{
-                    "secretIdentities":data.secretIdentities.split(","),
-                    "birthPlace": data.birthPlace,
-                    "occupation":data.occupation,
-                    "aliases":data.aliases.split(','),
-                    "alignment":data.alignment,
-                    "firstAppearance":data.firstAppearance,
-                    "yearAppearance":data.yearAppearance,
-                    "universe":data.universe,
+            "id":data.id,
+            "name":data.name,
+            "description":data.description,
+            "imageUrl":data.imageUrl,
+            "backgroundImageUrl":data.backgroundImageUrl,
+            "externalLink":data.externalLink,
+            "identity":{
+                "secretIdentities":data.secretIdentities.split(","),
+                "birthPlace": data.birthPlace,
+                "occupation":data.occupation,
+                "aliases":data.aliases.split(','),
+                "alignment":data.alignment,
+                "firstAppearance":data.firstAppearance,
+                "yearAppearance":data.yearAppearance,
+                "universe":data.universe,
 
-                },
-                "appearance":{
-                    "gender":data.gender,
-                    "race":data.race,
-                    "type":data.type,
-                    "height":data.height,
-                    "weight":data.weight,
-                    "eyeColor":data.eyeColor,
-                    "hairColor":data.hairColor
-                },
-                "teams":data.teams.split(','),
-                "powers":data.powers.split(','),
-                "partners":data.partners.split(','),
-                "skills":{
-                    "intelligence":data.intelligence,
-                    "strength":data.strength,
-                    "speed":data.speechSynthesis,
-                    "durability":data.durability,
-                    "power":data.power,
-                    "combat":data.combat   
-                },
-                "creators":data.creators.split(',')
-            });
-        })
-        // A la fin on créé l'ensemble des acteurs dans MongoDB
-        .on('end', () => {
-            collection.insertMany(heroes, (err, result) => {
-                callback(result);
-            });
+            },
+            "appearance":{
+                "gender":data.gender,
+                "race":data.race,
+                "type":data.type,
+                "height":data.height,
+                "weight":data.weight,
+                "eyeColor":data.eyeColor,
+                "hairColor":data.hairColor
+            },
+            "teams":data.teams.split(','),
+            "powers":data.powers.split(','),
+            "partners":data.partners.split(','),
+            "skills":{
+                "intelligence":data.intelligence,
+                "strength":data.strength,
+                "speed":data.speechSynthesis,
+                "durability":data.durability,
+                "power":data.power,
+                "combat":data.combat
+            },
+            "creators":data.creators.split(',')
         });
+})
+    // A la fin on créé l'ensemble des acteurs dans MongoDB
+.on('end', () => {
+        collection.insertMany(heroes, (err, result) => {
+            callback(result);
+});
+});
 }
 
 MongoClient.connect(mongoUrl, (err, client) => {
@@ -72,8 +72,8 @@ MongoClient.connect(mongoUrl, (err, client) => {
         throw err;
     }
     const db = client.db(dbName);
-    insertHeroes(db, result => {
-        console.log(`${result.insertedCount} actors inserted`);
-        client.close();
-    });
+insertHeroes(db, result => {
+    console.log(`${result.insertedCount} actors inserted`);
+client.close();
+});
 });
