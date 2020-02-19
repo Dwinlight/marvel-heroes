7f2fcaef59eb8ba945187da3d4780380c2bdcc3d
Dwinlight <corentinpouget15@gmail.com>
2020-02-19 15:36:58 +0100

    elasticsearch

--- app/repository/ElasticRepository.java
+++ app/repository/ElasticRepository.java
@@ -1,14 +1,17 @@
 package repository;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import env.ElasticConfiguration;
 import env.MarvelHeroesConfiguration;
 import models.PaginatedResults;
 import models.SearchedHero;
+import play.libs.Json;
 import play.libs.ws.WSClient;
 import utils.SearchedHeroSamples;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -28,13 +31,26 @@ public class ElasticRepository {
 
 
     public CompletionStage<PaginatedResults<SearchedHero>> searchHeroes(String input, int size, int page) {
-        return CompletableFuture.completedFuture(new PaginatedResults<>(3, 1, 1, Arrays.asList(SearchedHeroSamples.IronMan(), SearchedHeroSamples.MsMarvel(), SearchedHeroSamples.SpiderMan())));
-        // TODO
-        // return wsClient.url(elasticConfiguration.uri + "...")
-        //         .post(Json.parse("{ ... }"))
-        //         .thenApply(response -> {
-        //             return ...
-        //         });
+        String in = (input == "" ? "*": input.trim());
+        //return CompletableFuture.completedFuture(new PaginatedResults<>(3, 1, 1, Arrays.asList(SearchedHeroSamples.IronMan(), SearchedHeroSamples.MsMarvel(), SearchedHeroSamples.SpiderMan())));
+
+
+
+        return wsClient.url(elasticConfiguration.uri +"/heroes/_search")
+                 .post(Json.parse("{\"query\": {\"query_string\": {\"query\": \""+in+"\",\"fields\": [\"name^3\", \"secretIdentities^2\", \"secretIdentities^2\", \"aliases^2\"  ]} }, \"size\": "+size +", \"from\":"+size*(page-1)+"}"))
+                 .thenApply(response -> {
+                     JsonNode json = Json.parse(response.getBody()).get("hits").get("hits");
+                     System.out.println(json);
+                     List<SearchedHero> heros = new ArrayList<>();
+                     json.forEach(h -> {
+                            System.out.println(h.get("_source"));
+                                 heros.add(SearchedHero.fromJson(h.get("_source")));
+
+                             }
+                             );
+                    System.out.println(heros.size());
+                     return new PaginatedResults<>(heros.size(),page,(heros.size()/size)+1,heros);
+                });
     }
 
     public CompletionStage<List<SearchedHero>> suggest(String input) {
--- scripts/import-elasticsearch.js
+++ scripts/import-elasticsearch.js
@@ -2,8 +2,75 @@ const csv = require('csv-parser');
 const fs = require('fs');
 
 const { Client } = require('@elastic/elasticsearch')
-const esClient = new Client({ node: 'http://localhost:9200' })
+const client = new Client({ node: 'https://elastic:QHeuTPF3kQQ5Xxw0O2HGX3WQ@261265004d634c70a0389c639eaa3108.europe-west2.gcp.elastic-cloud.com:9243' })
 const heroesIndexName = 'heroes'
 
-// TODO
-console.log("TODO ;-)");
+// Création de l'indice
+
+async function run () {
+
+
+    const body = await client.indices.exists({
+        index: heroesIndexName
+    })
+    console.log(body.statusCode);
+    if (body.statusCode == 200) {
+        console.log("suppression");
+        await client.indices.delete({
+            index: "heroes"
+        });
+    }
+
+
+    client.indices.create({index: heroesIndexName}, (err, resp) => {
+        if(err) console.trace(err.message);
+    });
+
+    let heroes = [];
+    fs
+        .createReadStream('all-heroes.csv')
+        .pipe(csv())
+        // Pour chaque ligne on créé un document JSON pour l'acteur correspondant
+        .on('data', data => {
+        heroes.push({
+            "id" : data.id,
+            "thumbnail": data.imageUrl,
+            "name": data.name,
+            "aliases": data.aliases.split(','),
+            "secretIdentities": data.secretIdentities.split(","),
+            "universe": data.universe,
+            "partners": data.partners.split(','),
+            "description": data.description
+        });
+})
+// A la fin on créé l'ensemble des acteurs dans ElasticSearch
+.
+    on('end', () => {
+        client.bulk(createBulkInsertQuery(heroes), (err, resp) => {
+            if(err) console.trace(err.message);
+else
+    console.log(resp.body.items.length);
+    client.close();
+})
+    ;
+})
+    ;
+
+// Fonction utilitaire permettant de formatter les données pour l'insertion "bulk" dans elastic
+    function createBulkInsertQuery(heroes) {
+        const body = heroes.reduce((acc, heroes) => {
+            const {id,thumbnail,name, aliases, secretIdentities, universe, partners, description} = heroes;
+        acc.push({index: {_index: heroesIndexName}})
+        acc.push({id,thumbnail  ,name, aliases, secretIdentities, universe, partners, description})
+        return acc
+    },
+        []
+    )
+        ;
+
+        return {body};
+    }
+}
+
+
+run().catch(console.error);
